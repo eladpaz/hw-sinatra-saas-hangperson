b3a0cb2282849c92a054271093c0b21b98a0f112
eladp <eladbaz@gmail.com>
2015-11-25 21:54:07 +0000

    hurrah, all green!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,10 +8,50 @@ class HangpersonGame
   # def initialize()
   # end
   
-  def initialize(word)
+  attr_accessor :wrong_guesses, :word, :guesses
+  
+  def initialize(word='')
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+    @num_of_guesses = 0
   end
+  
+ # def guessing(letter)
+  #  guess(letter)
+  #end
+  
+  def guess(letters)
+    
+    raise ArgumentError, "Argument error!" if letters == nil or letters == '' or !letters.match(/[[:alpha:]]/)
+      
+    letters.each_char do  |letter|
+      @num_of_guesses += 1
+      letter.downcase!
+      if guesses[letter] or wrong_guesses[letter]
+        puts "you already guessed that letter"
+        return false
+      elsif word[letter]
+        guesses<< letter
+      else
+        wrong_guesses<< letter 
+      end
+    end
+    
+  end
+  
+ def word_with_guesses
+   return word.gsub(/[a-zA-Z]/,'-') if @guesses.empty? 
+   return word.gsub(/[^#{guesses}]/,'-')
+ end
 
+  def check_win_or_lose
+    return :lose if @num_of_guesses >= 7
+    return :win if word.gsub(/[#{guesses}]/,'')==''
+  
+    return :play
+  end
+  
   def self.get_random_word
     require 'uri'
     require 'net/http'
